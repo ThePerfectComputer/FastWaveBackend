0897bbae663b427d1d7d5562955aa252cadc8a14
Oscar Gustafsson <oscar.gustafsson@gmail.com>
2023-09-13 13:24:13 +0200

    Keep index as part of signal name

--- src/vcd/parse/scopes.rs
+++ src/vcd/parse/scopes.rs
@@ -98,11 +98,7 @@ pub(super) fn parse_var<'a, R: std::io::Read>(
         let (word, _) = next_word!(word_reader)?;
         match word {
             "$end" => break,
-            other => {
-                if !other.starts_with("[") {
-                    full_signal_name.push(word.to_string())
-                }
-            }
+            _ => full_signal_name.push(word.to_string())
         }
     }
     let full_signal_name = full_signal_name.join(" ");
